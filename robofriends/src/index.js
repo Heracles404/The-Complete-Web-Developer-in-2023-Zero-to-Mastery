7f79a6ae4db8a78a200977262eebb512c20fab52
Nazareno Bianca <103508458+Heracles404@users.noreply.github.com>
2023-06-15 18:52:11 +0800

    robofriends init & hello.js and hello.css

--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
+import Hello from './Hello';
+import 'tachyons';
 import reportWebVitals from './reportWebVitals';
-import Hello from "./Hello";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Hello greeting={'Hello React Ninja'}/>);
+root.render(
+  <React.StrictMode>
+    <h1>Hello World</h1>
+      <Hello greeting={'Hello ' + 'React Ninja'}/>
+      <App/>
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
