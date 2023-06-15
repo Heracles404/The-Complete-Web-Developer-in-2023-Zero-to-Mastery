d975361ee339af7b218402fcbad38beef59b0c28
Nazareno Bianca <103508458+Heracles404@users.noreply.github.com>
2023-06-15 19:04:59 +0800

    packages

--- background-generator/script.js
+++ background-generator/script.js
@@ -1,3 +1,7 @@
+import {without} from "lodash";
+
+var array = [1, 2, 3, 4, 5, 6 ,7, 8];
+console.log('Answer:', _.without(array, 3))
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color-1");
 var color2 = document.querySelector(".color-2");
