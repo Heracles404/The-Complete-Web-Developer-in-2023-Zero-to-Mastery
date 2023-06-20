c96c8abcc76481e0ffb95e00190812e0540575da
Nazareno Bianca <103508458+Heracles404@users.noreply.github.com>
2023-06-20 11:47:31 +0800

    RoboFriends Organized

--- robofriends/src/containers/App.js
+++ robofriends/src/containers/App.js
@@ -1,8 +1,8 @@
 import React, {Component} from "react";
-import CardList from "./CardList";
-import {robots} from "./robots";
-import SearchBox from "./SearchBox";
-
+import CardList from "../components/CardList";
+import {robots} from "../robots";
+import SearchBox from "../components/SearchBox";
+import Scroll from '../components/Scroll';
 
 class App extends Component{
     // Determines initial state
@@ -27,15 +27,18 @@ class App extends Component{
     // DOM Manipulation
     render(){
         // Filters Robots
-        const filteredRobots = this.state.robots.filter(robots => {
-            return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+        const {robots} = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
         })
 
         return(
             <div className='tc'>
                 <h1 className='f1'>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots}/>
+                <Scroll>
+                    <CardList robots={filteredRobots}/>
+                </Scroll>
             </div>
         );
     }
--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './App.css';
+import './containers/App.css';
 import 'tachyons';
-import App from './App';
+import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 
 const rootElement = document.getElementById('root');
