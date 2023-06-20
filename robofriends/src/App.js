2731ecbe629488ed43648d86c64c01d1b518062c
Nazareno Bianca <103508458+Heracles404@users.noreply.github.com>
2023-06-20 10:47:48 +0800

    RoboFriends Non Responsive

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,30 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-import {render} from "@testing-library/react";
-import React from "react";
+import React, {Component} from "react";
+import CardList from "./CardList";
+import {robots} from "./robots";
+import SearchBox from "./SearchBox";
 
-class App extends React.Component{
-  render() {
-      return (
-          <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                Edit <code>src/App.js</code> and save to reload.
-              </p>
-              <a
-                  className="App-link"
-                  href="https://reactjs.org"
-                  target="_blank"
-                  rel="noopener noreferrer"
-              >
-                Learning React
-              </a>
-            </header>
-          </div>
-      )
-  }
 
+class App extends Component{
+    // Determines initial state
+    constructor() {
+        super()
+        this.state = {
+            robots: [],
+            searchField: ''
+        }
+    }
+
+    componentDidMount() {
+        this.setState({robots: robots})
+    }
+
+    // Detects Changes TextField Value
+    onSearchChange = (event) => {
+        this.setState({searchField: event.target.value })
+    }
+
+
+    // DOM Manipulation
+    render(){
+        // Filters Robots
+        const filteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+        })
+
+        return(
+            <div className='tc'>
+                <h1 className='f1'>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={filteredRobots}/>
+            </div>
+        );
+    }
 }
 
 export default App;
--- robofriends/src/index.js
+++ robofriends/src/index.js
@@ -1,21 +1,11 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import Hello from './Hello';
+import './App.css';
 import 'tachyons';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <h1>Hello World</h1>
-      <Hello greeting={'Hello ' + 'React Ninja'}/>
-      <App/>
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const rootElement = document.getElementById('root');
+ReactDOM.createRoot(rootElement).render(<App />);
 reportWebVitals();
